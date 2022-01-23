cfd8262db4fddb509b4321fbe44cc440579e894c
dumbu1 <krishnamohan.balivada@gmail.com>
2022-01-23 11:50:38 +0530

    screenshot without listners using TakeScreenShot interface

--- screenshotUsingInterface/src/test/java/module/TakingScreenshot.java
+++ screenshotUsingInterface/src/test/java/module/TakingScreenshot.java
@@ -1,39 +1,28 @@
 package module;
 
-import java.io.File;
-import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TakingScreenshot {
-	
+
 	@Test
-	public void TakingScreenShot() {
+	public void takingScreenShot() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver wd=new ChromeDriver();
 		wd.get("http://the-internet.herokuapp.com/");
 		
-		//taking screenshot
-		//add commons-io dependency
+		CommonMethods obj_CommonMethods=new CommonMethods();
 		
-		TakesScreenshot obj_TakesScreenshot=(TakesScreenshot)wd;
-		File sourceFile=obj_TakesScreenshot.getScreenshotAs(OutputType.FILE);
-		
-		File destinationFile=new File(System.getProperty("user.dir")+File.separator+"screenshots"+File.separator+"1.png");
+		//obj_CommonMethods.getScreenshot(wd, "takingScreenShot");
 		
 		
-		try {
-			FileUtils.copyFile(sourceFile,destinationFile);
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+		
 	}
 
 }
